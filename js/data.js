eeabbe91aa582b5c49b3c09577254df93463d726
Cole Pergerson <cole.pergerson@gmail.com>
2023-01-07 17:37:37 -0800

    completed version 1

--- js/data.js
+++ js/data.js
@@ -4,38 +4,112 @@
 
 let data = `
 
+#template:
+\"<a> @mood@ game set @setting@, where you play as <a> @character@ who must use <a> combination of @mechanic@ and @mechanic@ mechanics to @goal@. The game must @constraint@.\"
+#end
 
-#mechanics:
-shoot
-jump
-shoot lasers
-swing your sword
-gain or lose health
-run
-stop
-dodge
-combine X
-pick up X
-have to carry X
-fly
-fall
-move
-throw X
-switch actions or items
-attract X
-repel X
-propel yourself
-launch yourself
-explode
-gain or lose power
-destroy
-die
-turn in to X
-revive
-float up
-charge X
-kill X
-see X
+#mood:
+noir
+dark fantasy
+sci-fi
+brutal
+horror
+suspenseful
+upbeat
+comforting
+light-hearted
+silly
+#end
+
+#setting:
+inside <a> @setting_description@ [underground bunker, arctic base, mountain keep]
+in <a> @setting_description@ [castle, city]
+#end
+
+#setting_description:
+haunted
+ancient
+old
+cyberpunk
+#end
+
+#character:
+detective
+solider
+vampire hunter
+ghost
+secret agent
+machine
+samurai
+knight
+night creature
+alien
+mech pilot
+ace-pilot
+marine sargent
+#end
+
+#character_description:
+rogue
+sarcastic
+wondering
+famous
+evil
+young
+courageous
+#end
+
+#mechanic:
+melee combat
+shooting
+jumping
+endless running
+dodging
+picking up
+carrying
+flying
+falling
+running
+throwing
+attracting
+repelling
+propelling
+repeatedly dying
+explosions
+floating
+charging
+killing
+line of sight
+pattern movement
+chance
+trading
+stealth
+beat-em up
+character customization
+spell casting
+worker placement
+hide-in-seek
+inventory management
+survival
+economy building
+#end
+
+#goal:
+seek revenge
+find true love
+find the truth
+save a friend
+#end
+
+#constraint:
+be a 1 bit game
+include volumetric lighting
+include multiple endings
+include visually stunning grass
+use a level up progression
+use multiple balanced items
+include character upgrades
+use cinematic camera angles
 #end
 
 `;
--- js/generator.js
+++ js/generator.js
@@ -4,17 +4,127 @@ $(document).ready(function() {
 
 function generateIdea()
 {
-    let list = getListAsArray('mechanics');
-    let mechanic1 = pickRandom(list);
-    let mechanic2 = pickRandom(list);
 
-    $('#idea').text(`Make a game where you ${mechanic1} when you ${mechanic2}.`);
+    
+    let template = pickRandom('template');
+
+    let result = fillInTemplate(template);
+
+    $('#idea').text(result);
 }
 
-function pickRandom(list)
+function fillInTemplate(template)
 {
+    // if includes @
+        // get text between @ and @
+        // find function for generator found
+        // replace text
+        // call fillInTemplate
+    if(template.includes('@'))
+    {
+        let generator = getTextBetweenTags(template, '@','@');
+        let replacement = 'NO TEXT FOUND';
+
+        switch(generator)
+        {
+            case 'mood':
+                replacement = generateMood();
+                break;
+            case 'character':
+                replacement = generateCharacter();
+                break;
+            case 'setting':
+                replacement = generateSetting();
+                break;
+            case 'setting_description':
+                replacement = generateSettingDescription();
+                break;
+            case 'mechanic':
+                replacement = generateMechanic();
+                break;
+            case 'goal':
+                replacement = generateGoal();
+                break;
+            case 'constraint':
+                replacement = generateConstraint();
+                break;
+
+        }
+
+        template = replaceTextBetweenTags(template, replacement, '@','@');
+
+        //console.log(template);
+        return fillInTemplate(template);
+    }
+    
+    // if includes <a>
+        // get first word after <a>
+        // pick either a or an
+        // call fillInTemplate
+    if(template.includes('<a>'))
+    {
+        let startIndex = template.indexOf('>') + 2;
+        let nextWord = template.substring(startIndex);
+        let replacement = indefiniteArticle(nextWord);
+        
+        template = replaceTextBetweenTags(template, replacement, '<','>');
+
+        return fillInTemplate(template);
+    }
+    
+    // if includes (
+        // pick conjugation of verb based on character being singular or plural
+        // call fillInTemplate
+    return template;
+}
+
+function generateMood()
+{
+    return pickRandom('mood');
+}
+
+
+function generateCharacter()
+{
+    let preDescription = pickRandom('character_description');
+    let character = pickRandom('character');
+
+    return ' <a> ' + preDescription + ' ' + character;
+}
+
+function generateGoal()
+{
+    return pickRandom('goal');
+}
+
+function generateSetting()
+{
+    return pickRandom('setting');
+}
+
+function generateSettingDescription()
+{
+    return pickRandom('setting_description');
+}
+
+function generateMechanic()
+{
+    return pickRandom('mechanic');
+}
+
+function generateConstraint()
+{
+    return pickRandom('constraint');
+}
+
+
+function pickRandom(listName)
+{
+    let list = getListAsArray(listName);
     let randomIndex = Math.floor(Math.random() * list.length);
-    return list[randomIndex];
+    let result = resolveOptions(list[randomIndex]);
+
+    return result;
 }
 
 function getListAsArray(listName)
@@ -24,7 +134,53 @@ function getListAsArray(listName)
     return getTextBetweenTags(data, start, end).split('\n');
 }
 
+function getRandomFromList(list)
+{
+    let randomIndex = Math.floor(Math.random() * list.length);
+    return list[randomIndex];
+}
+
+function resolveOptions(text)
+{
+    if(text.includes('['))
+    {
+        let list = getTextBetweenTags(text, '[',']');
+        let randomChoice = getRandomFromList(list.split(','));
+        text = replaceTextBetweenTags(text, randomChoice, '[',']');
+        return resolveOptions(text);
+    }
+
+    return text;
+    
+}
+
 function getTextBetweenTags(text, startTag, endTag)
 {
     return text.split(startTag)[1].split(endTag)[0];
+}
+
+function replaceTextBetweenTags(text, replacement, startTag, endTag)
+{
+    let startIndex = text.indexOf(startTag);
+    let endIndex = text.indexOf(endTag, startIndex+1);
+
+    return text.substring(0,startIndex) + replacement + text.substring(endIndex+1);
+}
+
+function indefiniteArticle(word)
+{
+    word = word.trim();
+
+	if (word.startsWith('one') || word.startsWith('uni')) {
+		return 'a';
+	}
+
+    let vowels = 'aeiou';
+
+	if (vowels.includes(word[0])) {
+		return 'an';
+	}
+	
+	return 'a';
+
 }
